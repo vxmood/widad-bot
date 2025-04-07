c9b79616204436ee19ffc6680aa096d6903e1331
vxmood <alwidadmohammed@gmail.com>
2025-04-07 10:26:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,8 @@ def bot():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
