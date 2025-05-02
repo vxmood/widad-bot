21d78ec3a0a831f9fdeaae65bc9e81d8d1ae544a
vxmood <alwidadmohammed@gmail.com>
2025-05-02 14:28:48 +0400

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def load_ai_model():
             device_map="auto",
             load_in_8bit=True,  # تقليل استخدام الذاكرة
             torch_dtype=torch.float16
+            low_cpu_mem_usage=True 
         )
         
         return pipeline(
